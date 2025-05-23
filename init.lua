7857185b5ad6141248e768568988510e47b60c98
GJ Tiquia <gershom.tiquia@gmail.com>
2025-05-23 15:38:29 -0400

    setup csharpier

--- init.lua
+++ init.lua
@@ -741,6 +741,16 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+
+        -- (GJ) formatting for csharp
+        cs = { 'csharpier' },
+      },
+      formatters = {
+        csharpier = {
+          command = 'csharpier',
+          args = { 'format', '--write-stdout' },
+          stdin = true,
+        },
       },
     },
   },
