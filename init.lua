4fb5410cb862dc5ebc5d5850212c2faa5b951f9a
GJ Tiquia <gershom.tiquia@gmail.com>
2025-05-26 20:24:14 -0400

    change keymappings for document/workspace symbols

--- init.lua
+++ init.lua
@@ -536,11 +536,13 @@ require('lazy').setup({
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+          -- (GJ) changed keymapping from gO to gro, cuz im always on gr- anyways for other LSP stuff
+          map('gro', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+          -- (GJ) changed keymapping from gW to grw
+          map('grw', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
