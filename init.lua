03da426c53676a490be8926adf1096417824ea6f
GJ Tiquia <gershom.tiquia@gmail.com>
2025-05-27 23:47:15 -0400

    added vim-startify startup screen plugin

--- init.lua
+++ init.lua
@@ -198,6 +198,10 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+
+  -- (GJ) startup screen
+  'mhinz/vim-startify',
+
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
