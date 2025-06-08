4495e1a3fe40d0fdf4dc6c35448a3bcb3d7af14b
GJ Tiquia <gershom.tiquia@gmail.com>
2025-06-08 09:23:40 -0400

    ensure roslyn installed for LSP in Mason

--- init.lua
+++ init.lua
@@ -836,6 +836,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'roslyn', -- (GJ) Used to format C# code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
