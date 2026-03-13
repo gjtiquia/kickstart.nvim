d00924a485bb0efbdccf3dd2606617a2e95cef2e
GJ Tiquia <gjtiquia@gmail.com>
2026-03-13 01:04:13 -0700

    installed folke/zen-mode.nvim

--- init.lua
+++ init.lua
@@ -1576,6 +1576,24 @@ require('lazy').setup({
     'github/copilot.vim',
   },
 
+  -- (GJ) zen mode
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      window = {
+        options = {
+          -- not sure why not working
+          -- number = true,
+          -- relativenumber = true,
+        },
+      },
+      plugins = {
+        gitsigns = { enabled = true },
+        tmux = { enabled = true },
+      },
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
